776755c9beb8c197b75cc96687f0ccef40eb288b
Tsonewa <tsonewa@gmail.com>
2022-01-23 16:05:47 +0200

    depth traversing impl

--- DataStructures/Trees-Representation/src/main/java/Main.java
+++ DataStructures/Trees-Representation/src/main/java/Main.java
@@ -1,5 +1,30 @@
+import implementations.Tree;
+
 public class Main {
     public static void main(String[] args) {
 
+        Tree<Integer> tree =
+                new Tree<>(1,
+                        new Tree<>(11),
+                        new Tree<>(111),
+                new Tree<>(2,
+                        new Tree<>(3,
+                        new Tree<>(4))));
+
+        for (Integer e: tree.orderDfs()) {
+            System.out.println(e);
+        }
+
+//     printTree(tree);
+
     }
+    //DFS
+//    private static void printTree(Tree<Integer> tree) {
+//        System.out.println(tree.getKey());
+//
+//        for (Tree<Integer> node: tree.getChildren()) {
+//            printTree(node);
+//        }
+//
+//    }
 }
--- DataStructures/Trees-Representation/src/main/java/implementations/Tree.java
+++ DataStructures/Trees-Representation/src/main/java/implementations/Tree.java
@@ -12,23 +12,60 @@ public class Tree<E> implements AbstractTree<E> {
     private List<Tree<E>> children;
 
 
+    public E getKey() {
+        return key;
+    }
+
+    public void setKey(E key) {
+        this.key = key;
+    }
+
+    public Tree<E> getRoot() {
+        return root;
+    }
+
+    public void setRoot(Tree<E> root) {
+        this.root = root;
+    }
+
+    public List<Tree<E>> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Tree<E>> children) {
+        this.children = children;
+    }
+
     public Tree(E key, Tree<E>...children) {
        this.key = key;
        this.children = new ArrayList<>();
         for (Tree<E> child: children) {
             this.children.add(child);
             child.root = this;
+            this.root = child.root;
         }
     }
 
     @Override
     public List<E> orderBfs() {
+
         return null;
     }
 
     @Override
     public List<E> orderDfs() {
-        return null;
+
+        List<E> result = new ArrayList<>();
+        this.dfs(this.root, result);
+        return result;
+    }
+
+    private void dfs(Tree<E> node, List<E> result) {
+        result.add(node.getKey());
+
+        for (Tree<E> child: node.children) {
+            dfs(child, result);
+        }
     }
 
     @Override
